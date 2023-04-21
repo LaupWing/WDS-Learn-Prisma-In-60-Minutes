f22367c8de2718a40df7edca921e08bd3e401145
LaupWing <loc_strict@hotmail.com>
2023-04-21 05:41:09 +0200

    update

--- script.ts
+++ script.ts
@@ -2,13 +2,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-   // const user = await prisma.user.create({
-   //    data: {
-   //       name: "Laup Wing"
-   //    }
-   // })
-
-   // console.log(user)
+   await prisma.user.deleteMany()
 }
 
 main()
